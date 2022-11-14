4e2418e69ddffe8882ffcec0f9580b5afd9a6d15
Nawang Yolmo <97735450+designtribestudio@users.noreply.github.com>
2022-11-14 15:43:06 +0530

    web version ready

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 window.onscroll = function() {scrollFunction()};
 var logo = document.getElementById('xen-logo');
+var icon = document.getElementById('iconMobile');
 function scrollFunction() {
-  if (document.body.scrollTop > 870 || document.documentElement.scrollTop > 850) {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
     document.getElementById("nb").className = "dark";
     logo.src='./assets/Xenesis-dark.svg';
+    icon.src='./assets/menu-dark.svg';
   } else {
     document.getElementById("nb").className = "navbar";
     logo.src='./assets/Xenesis.svg';
+    icon.src='./assets/menu.svg';
   }
 }
